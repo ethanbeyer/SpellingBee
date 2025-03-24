02924a574a4a67bdacaba3d35f713b5887086923
Ethan Beyer <ethan@ethanbeyer.com>
2025-03-24 09:56:08 -0400

    basic route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\WordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); // @todo -- ask for letters
 });
+
+Route::get('/words/{letters}', [WordController::class, 'find_words'])->where('letters', '[a-zA-Z]{7}');
