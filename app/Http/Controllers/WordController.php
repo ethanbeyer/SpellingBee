636b0dc9f92c51ed87558e45de5418ddd70d1b4d
Ethan Beyer <ethan@ethanbeyer.com>
2025-03-24 14:27:13 -0400

    updates the controller to finish the letter-by-letter breakdown

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -49,21 +49,24 @@ class WordController extends Controller
 
         // letter-by-letter breakdowns
         foreach($letters_array as $letter) {
-            $words_with_this_letter = $all_words->filter(function(int $value, int $key) {
-                dd('HERE');
+
+            // get the words from the list that contain this letter
+            $words_with_this_letter = $all_words->filter(function(Word $value, int $key) use ($letter) {
                 return str_contains($value->word, $letter);
             });
-
-            // dd($words_with_this_letter);
             
+            // with that list, totals:
+            // A. How many words from the list use this letter
+            // 2. The Maximum Score for this letter
+            // D. The list of words
             $word_breakdown[$letter] = [
                 'count' => $words_with_this_letter->count(),
-                'score' => $words_with_this_letter->sum('score')
+                'score' => $words_with_this_letter->sum('score'),
+                'words' => $words_with_this_letter->pluck('word')->all()
             ];
         }
 
-        die('oh no');
-
+        // data for the view
         $data = [
             'letters' => $letters_array,
             'all_words' => $all_words,
